b6d6f47f16a6bf8b56a0d3d8c5035c4ef5b31994
Sandip Nepali <100818527+sandeepsz@users.noreply.github.com>
2025-02-05 14:24:21 +0000

    feat:complete user auth and setup for message

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,22 @@
 import dotenv from "dotenv";
 import express from "express";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./src/lib/db.js";
 import authRoutes from "./src/routes/auth.route.js";
+import messageRoute from "./src/routes/message.route.js"
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3001;
+const HOST = "localhost";
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoute)
 
 app.listen(PORT, () => {
-  console.log("Server is running on port " + PORT);
+  console.log(`Server is running at http://${HOST}:${PORT}`);
   connectDB();
 });
